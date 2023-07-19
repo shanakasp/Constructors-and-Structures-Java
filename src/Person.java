e1c99e1ccec9e665698b435763703691468188ea
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-19 10:31:01 +0530

    modified 3 files

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private String name;
+    private int age;
+
+    // Parameterized constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Default constructor
+    public Person() {
+        this.name = "";
+        this.age = 0;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        Person person1 = new Person("shanaka", 30);
+        Person person2 = new Person("prince", 25);
+
+        System.out.println("Person 1: Name: " + person1.getName() + ", Age: " + person1.getAge());
+        System.out.println("Person 2: Name: " + person2.getName() + ", Age: " + person2.getAge());
+
+    }
 }
--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    private int x;
+    private int y;
+
+    // Parameterized constructor
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getter methods
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    // Setter methods (optional, not used in this example)
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public static void main(String[] args) {
+        // Create two Point objects using the constructor
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(5, 7);
+
+        // Accessing the data members using getter methods
+        System.out.println("Point 1: x = " + p1.getX() + ", y = " + p1.getY());
+        System.out.println("Point 2: x = " + p2.getX() + ", y = " + p2.getY());
+    }
 }
